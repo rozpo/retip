a647c90b07915f8cd0aec9f3d6b539c782867bd3
Piotr Rozpończyk <rozpo.dev@gmail.com>
2024-08-28 23:18:44 +0100

    Workaround for release notification icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:just_audio_background/just_audio_background.dart';
@@ -18,9 +19,11 @@ void main() async {
 Future<void> init() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  const icon = kReleaseMode ? 'mipmap/ic_launcher' : 'drawable/ic_stat_name';
+
   await JustAudioBackground.init(
     androidNotificationChannelId: 'dev.rozpo.retip.audio',
-    androidNotificationIcon: 'drawable/ic_stat_name',
+    androidNotificationIcon: icon,
     androidStopForegroundOnPause: true,
     androidNotificationOngoing: true,
   );
