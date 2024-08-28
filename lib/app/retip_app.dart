73237fdc9659b7c7adc963237893a0588b099c49
Piotr Rozpończyk <rozpo.dev@gmail.com>
2024-08-28 08:46:03 +0100

    Create router navigation

--- lib/app/retip_app.dart
+++ lib/app/retip_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:retip/app/views/home_view.dart';
 import 'package:retip/core/l10n/retip_l10n.dart';
+import 'package:retip/core/router/retip_router.dart';
 import 'package:retip/core/theme/retip_theme.dart';
 
 class RetipApp extends StatelessWidget {
@@ -8,7 +8,7 @@ class RetipApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       themeMode: ThemeMode.system,
       theme: RetipTheme.light,
       darkTheme: RetipTheme.dark,
@@ -16,7 +16,7 @@ class RetipApp extends StatelessWidget {
       highContrastDarkTheme: RetipTheme.contrastDark,
       localizationsDelegates: RetipL10n.localizationsDelegates,
       supportedLocales: RetipL10n.supportedLocales,
-      home: const HomeView(),
+      routerConfig: RetipRouter.router,
     );
   }
 }
---
+++ lib/core/router/retip_router.dart
@@ -0,0 +1,15 @@
+import 'package:go_router/go_router.dart';
+import 'package:retip/app/views/home_view.dart';
+
+mixin RetipRouter {
+  static GoRouter get router => GoRouter(
+        routes: [
+          GoRoute(
+            path: '/',
+            builder: (context, state) {
+              return const HomeView();
+            },
+          ),
+        ],
+      );
+}
