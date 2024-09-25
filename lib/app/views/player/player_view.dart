47449a2f2a220abdcb9a3d717018907d91c54d1f
Piotr Rozpończyk <rozpo.dev@gmail.com>
2024-09-25 06:07:02 +0100

    Remove drawer from player view

--- lib/app/views/player/player_view.dart
+++ lib/app/views/player/player_view.dart
@@ -17,7 +17,6 @@ class PlayerView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      endDrawer: const Drawer(),
       appBar: AppBar(
         centerTitle: true,
       ),
