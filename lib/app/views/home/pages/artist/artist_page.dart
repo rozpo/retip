8548ad201edc9f3418703b1a9e6552a3a7054886
Piotr Rozpończyk <rozpo.dev@gmail.com>
2024-10-16 07:57:19 +0100

    Display all track on artist page

--- lib/app/views/home/pages/artist/artist_page.dart
+++ lib/app/views/home/pages/artist/artist_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:retip/app/services/entities/artist_entity.dart';
+import 'package:retip/app/services/entities/track_entity.dart';
 import 'package:retip/app/views/home/pages/album/album_page.dart';
+import 'package:retip/app/views/player/player_view.dart';
+import 'package:retip/core/audio/retip_audio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ArtistPage extends StatelessWidget {
+class ArtistPage extends StatefulWidget {
   final ArtistEntity artist;
 
   const ArtistPage({
@@ -11,55 +16,132 @@ class ArtistPage extends StatelessWidget {
   });
 
   @override
+  State<ArtistPage> createState() => _ArtistPageState();
+}
+
+class _ArtistPageState extends State<ArtistPage> {
+  @override
   Widget build(BuildContext context) {
+    final tracks = <TrackEntity>[];
+
+    for (final album in widget.artist.albums) {
+      for (final track in album.tracks) {
+        tracks.add(track);
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(artist.name),
+        title: Text(widget.artist.name),
       ),
-      body: GridView(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 8,
-        ),
-        children: artist.albums
-            .map(
-              (album) => GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return AlbumPage(album: album);
-                      },
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.width / 3,
+              child: ListView.separated(
+                separatorBuilder: (context, index) => const SizedBox(width: 8),
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: widget.artist.albums.length,
+                itemBuilder: (context, index) {
+                  final album = widget.artist.albums[index];
+
+                  return GestureDetector(
+                    onTap: () async {
+                      await Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return AlbumPage(album: album);
+                          },
+                        ),
+                      );
+
+                      setState(() {});
+                    },
+                    child: SizedBox.square(
+                      dimension: MediaQuery.of(context).size.width / 3,
+                      child: album.artwork != null
+                          ? Image.memory(
+                              album.artwork!,
+                              fit: BoxFit.cover,
+                            )
+                          : Container(
+                              color: Theme.of(context).colorScheme.primary,
+                              child: Text(
+                                album.title,
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(context).colorScheme.onPrimary,
+                                ),
+                              ),
+                            ),
                     ),
                   );
                 },
-                child: LayoutBuilder(
-                  builder: (context, constraints) => Column(
-                    children: [
-                      SizedBox.square(
-                        child: album.artwork != null
-                            ? Image.memory(
-                                album.artwork!,
-                                fit: BoxFit.cover,
-                              )
-                            : Container(
-                                color: Theme.of(context).colorScheme.primary,
-                                child: Text(
-                                  album.title,
-                                  style: TextStyle(
-                                    color:
-                                        Theme.of(context).colorScheme.onPrimary,
-                                  ),
-                                ),
-                              ),
-                      ),
-                    ],
-                  ),
-                ),
               ),
-            )
-            .toList(),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemCount: tracks.length,
+                itemBuilder: (context, index) {
+                  final prefs = GetIt.I.get<SharedPreferences>();
+                  final favouriteTracks =
+                      prefs.getStringList('favourite_tracks') ?? [];
+
+                  final track = tracks[index];
+                  track.isFavourite =
+                      favouriteTracks.contains(track.id.toString());
+
+                  return ListTile(
+                    leading: Text(track.index.toString().padLeft(2, '0')),
+                    title: Text(track.title),
+                    subtitle: Text(track.artist),
+                    trailing: IconButton(
+                      onPressed: () async {
+                        if (track.isFavourite) {
+                          favouriteTracks.remove(track.id.toString());
+                        } else {
+                          favouriteTracks.add(track.id.toString());
+                        }
+
+                        await prefs.setStringList(
+                            'favourite_tracks', favouriteTracks);
+
+                        setState(() {});
+                      },
+                      icon: Icon(
+                        track.isFavourite
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                      ),
+                    ),
+                    onTap: () async {
+                      final audio = GetIt.instance.get<RetipAudio>();
+
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => PlayerView(
+                            player: audio,
+                          ),
+                        ),
+                      );
+
+                      await audio.playlistAddAll(tracks);
+                      await audio.stop();
+                      await audio.seekToIndex(index);
+                      await audio.play();
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
