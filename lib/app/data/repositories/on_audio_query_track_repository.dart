9ea2fbad88e0c0bc83c3f2679861bde0965924d3
Piotr Rozpończyk <rozpo.dev@gmail.com>
2024-10-23 07:14:16 +0100

    Speed up loading tracks list

--- lib/app/data/repositories/on_audio_query_track_repository.dart
+++ lib/app/data/repositories/on_audio_query_track_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:retip/app/data/models/track_model.dart';
 import 'package:retip/app/data/providers/on_audio_query_provider.dart';
 import 'package:retip/app/services/repositories/track_repository.dart';
@@ -8,11 +10,27 @@ class OnAudioQueryTrackRepository implements TrackRepository {
   @override
   Future<List<TrackModel>> getAll() async {
     final data = await _onAudioQueryProvider.getAllTracks();
+
+    final artworks = <int, Uint8List>{};
     final tracks = <TrackModel>[];
 
-    for (final element in data) {
-      final artwork = await _onAudioQueryProvider.getAudioArtwork(element.id);
-      tracks.add(TrackModel.fromSongModel(element, artwork));
+    for (final track in data) {
+      final albumId = track.albumId;
+      Uint8List? artwork;
+
+      if (albumId != null) {
+        if (artworks.containsKey(albumId)) {
+          artwork = artworks[albumId];
+        } else {
+          artwork = await _onAudioQueryProvider.getAudioArtwork(track.id);
+
+          if (artwork != null) {
+            artworks.addAll({albumId: artwork});
+          }
+        }
+      }
+
+      tracks.add(TrackModel.fromSongModel(track, artwork));
     }
 
     return tracks;
