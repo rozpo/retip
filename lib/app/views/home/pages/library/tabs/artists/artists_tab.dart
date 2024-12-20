74374c51839c602ce58fc43dfc04ecbb72155c82
Piotr Rozpończyk <rozpo.dev@gmail.com>
2024-12-20 21:29:01 +0000

    Update artist card in grid view

--- lib/app/views/home/pages/library/tabs/artists/artists_tab.dart
+++ lib/app/views/home/pages/library/tabs/artists/artists_tab.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:retip/app/services/cases/get_all_artists.dart';
 import 'package:retip/app/services/entities/artist_entity.dart';
 import 'package:retip/app/views/artist/artist_view.dart';
+import 'package:retip/app/widgets/artwork_widget.dart';
+import 'package:retip/app/widgets/rp_text.dart';
+import 'package:retip/core/extensions/string_extension.dart';
 import 'package:retip/core/l10n/retip_l10n.dart';
 import 'package:retip/core/utils/sizer.dart';
 
@@ -40,88 +43,80 @@ class ArtistsTab extends StatelessWidget {
 
           data.removeWhere((artist) => artist.albums.isEmpty);
 
+          const columns = 2;
+
+          final textLineHeight = 'A'.height(
+              Theme.of(context).textTheme.bodySmall!,
+              MediaQuery.of(context).size.width / columns);
+          final textLineHeight2 = 'A'.height(
+              Theme.of(context).textTheme.bodyMedium!,
+              MediaQuery.of(context).size.width / columns);
+
           return GridView.builder(
             physics: const BouncingScrollPhysics(),
             // padding: const EdgeInsets.all(Sizer.x0_5),
             padding: const EdgeInsets.symmetric(
-                vertical: Sizer.x1 + Sizer.x0_5, horizontal: Sizer.x0_5),
+                vertical: Sizer.x1 + Sizer.x0_5, horizontal: Sizer.x1),
 
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisSpacing: Sizer.x1,
+              crossAxisSpacing: Sizer.x1,
+              crossAxisCount: columns,
+              mainAxisExtent: MediaQuery.of(context).size.width / columns +
+                  textLineHeight +
+                  textLineHeight2,
             ),
             itemCount: data.length,
             itemBuilder: (context, index) {
               final artist = data[index];
+              int tracksCount = 0;
 
-              return Padding(
-                padding: const EdgeInsets.all(Sizer.x0_5),
+              for (final album in artist.albums) {
+                tracksCount += album.tracks.length;
+              }
+
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ArtistView(artist: artist);
+                      },
+                    ),
+                  );
+                },
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.surfaceContainer,
                     borderRadius: BorderRadius.circular(Sizer.x1),
+                    color: Theme.of(context).colorScheme.surfaceContainer,
                   ),
-                  // color: Colors.red,
-                  child: Padding(
-                    padding: const EdgeInsets.all(Sizer.x1),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return ArtistView(artist: artist);
-                            },
-                          ),
-                        );
-                      },
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: CircleAvatar(
-                              radius: double.infinity,
-                              backgroundColor:
-                                  Theme.of(context).colorScheme.surfaceBright,
-                              child: Padding(
-                                padding: const EdgeInsets.all(2),
-                                child: CircleAvatar(
-                                  radius: double.infinity,
-                                  backgroundColor:
-                                      Theme.of(context).colorScheme.surface,
-                                  backgroundImage: artist.artwork != null
-                                      ? Image.memory(
-                                          artist.artwork!,
-                                          fit: BoxFit.cover,
-                                        ).image
-                                      : null,
-                                  child: artist.artwork == null
-                                      ? LayoutBuilder(
-                                          builder: (context, constraints) {
-                                          return SizedBox.square(
-                                              dimension: constraints
-                                                  .biggest.shortestSide,
-                                              child: Center(
-                                                  child: Icon(
-                                                Icons.person,
-                                                size: constraints
-                                                        .biggest.shortestSide /
-                                                    2,
-                                              )));
-                                        })
-                                      : null,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: Sizer.x0_5,
-                          ),
-                          Text(
-                            artist.name,
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context).textTheme.titleMedium,
-                          )
-                        ],
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ArtworkWidget(
+                        bytes: artist.artwork,
+                        borderWidth: 1,
                       ),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: Sizer.x0_5,
+                          horizontal: Sizer.x1,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RpText(
+                              artist.name,
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                            RpText(
+                              '${RetipL10n.of(context).albumsCount(artist.albums.length)} - ${RetipL10n.of(context).tracksCount(tracksCount)}',
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               );
---
+++ lib/core/extensions/string_extension.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+extension StringExtension on String {
+  double height(TextStyle style, double textWidth) {
+    final TextPainter textPainter = TextPainter(
+      text: TextSpan(text: this, style: style),
+      textDirection: TextDirection.ltr,
+      maxLines: 1,
+    )..layout(minWidth: 0, maxWidth: double.infinity);
+
+    final countLines = (textPainter.size.width / textWidth).ceil();
+    final height = countLines * textPainter.size.height;
+
+    return height;
+  }
+}
