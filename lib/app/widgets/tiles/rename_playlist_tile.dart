e77f1eb8e064790b2c49a466f2b6955ccbe401d2
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-01-03 18:44:15 +0000

    Update rename playlist dialog style

--- lib/app/widgets/tiles/rename_playlist_tile.dart
+++ lib/app/widgets/tiles/rename_playlist_tile.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:retip/app/services/cases/playlist/update_playlist.dart';
 import 'package:retip/app/services/entities/playlist_entity.dart';
 import 'package:retip/app/widgets/more/more_tile.dart';
+import 'package:retip/app/widgets/spacer.dart';
 import 'package:retip/core/l10n/retip_l10n.dart';
+import 'package:retip/core/utils/sizer.dart';
 
 class RenamePlaylistTile extends StatelessWidget {
   final PlaylistEntity entity;
@@ -27,6 +29,8 @@ class RenamePlaylistTile extends StatelessWidget {
         await showDialog<bool>(
           context: context,
           builder: (context) => AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(Sizer.x1)),
             content: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -36,31 +40,61 @@ class RenamePlaylistTile extends StatelessWidget {
                     text = value;
                   },
                 ),
+                const VerticalSpacer(),
+                const VerticalSpacer(),
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        style: ButtonStyle(
+                          padding:
+                              const WidgetStatePropertyAll(EdgeInsets.zero),
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(Sizer.x0_5),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(l10n.cancel),
+                      ),
+                    ),
+                    const HorizontalSpacer(),
+                    Expanded(
+                      child: FilledButton(
+                        style: ButtonStyle(
+                          padding:
+                              const WidgetStatePropertyAll(EdgeInsets.zero),
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(Sizer.x0_5),
+                            ),
+                          ),
+                        ),
+                        onPressed: () async {
+                          if (text.isNotEmpty) {
+                            entity.name = text;
+                            await UpdatePlaylist.call(entity);
+
+                            if (context.mounted) {
+                              Navigator.pop(context);
+                              Navigator.pop(context);
+                            }
+
+                            onTap?.call();
+                          }
+                        },
+                        child: Text(l10n.rename),
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
-            actions: [
-              OutlinedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text(l10n.cancel)),
-              FilledButton(
-                onPressed: () async {
-                  if (text.isNotEmpty) {
-                    entity.name = text;
-                    await UpdatePlaylist.call(entity);
-
-                    if (context.mounted) {
-                      Navigator.pop(context);
-                      Navigator.pop(context);
-                    }
-
-                    onTap?.call();
-                  }
-                },
-                child: Text(l10n.rename),
-              ),
-            ],
           ),
         );
       },
