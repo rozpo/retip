4a2c5ff8321b0adce5abd8179c91f9b64a01eb8d
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-01-09 21:12:49 +0000

    Fix shuffle mode

--- lib/app/domain/cases/play_audio.dart
+++ lib/app/domain/cases/play_audio.dart
@@ -1,6 +1,6 @@
 import 'package:get_it/get_it.dart';
-import 'package:retip/app/domain/entities/track_entity.dart';
 import 'package:retip/app/data/providers/retip_audio.dart';
+import 'package:retip/app/domain/entities/track_entity.dart';
 
 mixin PlayAudio {
   static void call(
@@ -10,11 +10,11 @@ mixin PlayAudio {
   }) async {
     final player = GetIt.I.get<RetipAudio>();
 
-    await player.playlistAddAll(tracks, index);
-
     if (shuffle != null) {
       await player.setShuffleMode(shuffle);
     }
+
+    await player.playlistAddAll(tracks, index);
 
     player.play();
   }
