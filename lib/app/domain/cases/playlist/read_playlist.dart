f6224370c7697be2eaf002a28ad407b54ae53737
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-01-16 22:50:21 +0000

    Speed up tracks reading for playlists

--- lib/app/domain/cases/playlist/read_playlist.dart
+++ lib/app/domain/cases/playlist/read_playlist.dart
@@ -1,25 +1,35 @@
 import 'package:get_it/get_it.dart';
 import 'package:retip/app/domain/cases/get_all_tracks.dart';
 import 'package:retip/app/domain/entities/playlist_entity.dart';
+import 'package:retip/app/domain/entities/track_entity.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 mixin ReadPlaylist {
   static Future<PlaylistEntity?> call(String key) async {
     final prefs = GetIt.I.get<SharedPreferences>();
 
-    final trackIds = prefs.getStringList(key);
-    if (trackIds == null) {
+    final values = prefs.getStringList(key);
+
+    if (values == null) {
       return null;
     }
+
+    final trackIds = values.sublist(1).map((e) => int.parse(e)).toList();
+
     final allTracks = await GetAllTracks.call();
 
-    final tracks = allTracks
-        .where((track) => trackIds.contains(track.id.toString()))
-        .toList();
+    final tracksMap = {
+      for (final track in allTracks) track.id: track,
+    };
+
+    final tracks = <TrackEntity>[];
+    for (final trackId in trackIds) {
+      tracks.add(tracksMap[trackId]!);
+    }
 
     return PlaylistEntity(
       id: int.parse(key.split('_')[1]),
-      name: trackIds.first,
+      name: values.first,
       tracks: tracks,
     );
   }
