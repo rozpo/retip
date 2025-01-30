1fb67284df2f32c85d743d5af6e96a8846ac106f
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-01-30 07:47:42 +0000

    Cleanup entry point

--- lib/main.dart
+++ lib/main.dart
@@ -17,61 +17,56 @@ import 'package:retip/app/retip_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  // Ensure that the widgets binding is initialized
   WidgetsFlutterBinding.ensureInitialized();
-
+  // Set the orientation to portrait
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-
-  const icon = 'drawable/ic_stat_name';
-
+  // Initialize the background audio player
   await JustAudioBackground.init(
     androidNotificationChannelId: 'dev.rozpo.retip.audio',
-    androidNotificationIcon: icon,
+    androidNotificationIcon: 'drawable/ic_stat_name',
     androidStopForegroundOnPause: true,
     androidNotificationOngoing: true,
   );
-
-  // Setup the dependency injection
+  // Setup shared preferences
   final sharedPrefs = await SharedPreferences.getInstance();
   GetIt.I.registerSingleton<SharedPreferences>(sharedPrefs);
-
+  // Setup package info
   final packageInfo = await PackageInfo.fromPlatform();
   GetIt.I.registerSingleton<PackageInfo>(packageInfo);
 
   // Register providers
   final onAudioQueryProvider = OnAudioQueryProvider();
-
-  final player =
-      GetIt.I.registerSingleton<JustAudioProvider>(JustAudioProvider());
-
-  final sharedPrefsProvider = GetIt.I.registerSingleton(
+  final justAudioProvider = GetIt.I.registerSingleton<JustAudioProvider>(
+    JustAudioProvider(),
+  );
+  final sharedPreferencesProvider = GetIt.I.registerSingleton(
     SharedPreferencesProvider(),
   );
 
-  // Library repository register
+  // Register repositories
   final libraryRepository = GetIt.I.registerSingleton<LibraryRepository>(
     LibraryRepositoryImplementation(
       onAudioQueryProvider: onAudioQueryProvider,
-      sharedPreferencesProvider: sharedPrefsProvider,
+      sharedPreferencesProvider: sharedPreferencesProvider,
     ),
   );
-
   final audioRepository = GetIt.I.registerSingleton<AudioRepository>(
     AudioRepositoryImplementation(
-      sharedPreferencesProvider: sharedPrefsProvider,
+      sharedPreferencesProvider: sharedPreferencesProvider,
       onAudioQueryProvider: onAudioQueryProvider,
       libraryRepository: libraryRepository,
-      justAudioProvider: player,
+      justAudioProvider: justAudioProvider,
     ),
   );
-
+  // Initialize the audio repository
   await audioRepository.init();
-
   // Run application
   runApp(RetipApp(
     debugRepository: DebugRepositoryImplementation(
-      sharedPreferencesProvider: sharedPrefsProvider,
+      sharedPreferencesProvider: sharedPreferencesProvider,
     ),
     libraryRepository: libraryRepository,
     permissionRepository: PermissionRepositoryImplementation(
@@ -79,7 +74,7 @@ void main() async {
     ),
     audioRepository: audioRepository,
     themeRepository: ThemeRepositoryImplementation(
-      provider: SharedPreferencesProvider(),
+      provider: sharedPreferencesProvider,
     ),
   ));
 }
