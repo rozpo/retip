2656416f01dbe4da8829d81bcc5fe14e5a4d170a
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-02-04 07:30:33 +0000

    Update bottom navigation bar background color

--- lib/app/presentation/widgets/organisms/bottom_navigation_bar_widget.dart
+++ lib/app/presentation/widgets/organisms/bottom_navigation_bar_widget.dart
@@ -35,26 +35,31 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
         },
         items: [
           BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
             activeIcon: const Icon(Icons.home),
             icon: const Icon(Icons.home_outlined),
             label: l10n.home,
           ),
           BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
             activeIcon: const Icon(Icons.explore),
             icon: const Icon(Icons.explore_outlined),
             label: l10n.explore,
           ),
           BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
             icon: const Icon(Icons.search_outlined),
             activeIcon: const Icon(Icons.search),
             label: l10n.search,
           ),
           BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
             icon: const Icon(Icons.library_music_outlined),
             activeIcon: const Icon(Icons.library_music),
             label: l10n.library,
           ),
           BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
             icon: const Icon(Icons.settings_outlined),
             activeIcon: const Icon(Icons.settings),
             label: l10n.settings,
