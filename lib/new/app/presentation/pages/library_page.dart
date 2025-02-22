c730413ef997d3205d2bffb2f4ff15a9addaa100
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-02-22 08:36:27 +0000

    Call library scan on page init

--- lib/new/app/presentation/pages/library_page.dart
+++ lib/new/app/presentation/pages/library_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../domain/usecases/library_usecase.dart';
 import '../views/albums_view.dart';
 import '../views/artists_view.dart';
 import '../views/genres_view.dart';
 import '../views/tracks_view.dart';
 
-class LibraryPage extends StatelessWidget {
+class LibraryPage extends StatefulWidget {
   const LibraryPage({super.key});
+
+  @override
+  State<LibraryPage> createState() => _LibraryPageState();
+}
+
+class _LibraryPageState extends State<LibraryPage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<LibraryUsecase>().call();
+  }
 
   @override
   Widget build(BuildContext context) {
