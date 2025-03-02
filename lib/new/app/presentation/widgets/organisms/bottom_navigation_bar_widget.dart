20d1171ae6cb759765143aa269811e6ed687e520
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-03-02 10:20:27 +0000

    No splash on bottom navbar

--- lib/new/app/presentation/widgets/organisms/bottom_navigation_bar_widget.dart
+++ lib/new/app/presentation/widgets/organisms/bottom_navigation_bar_widget.dart
@@ -16,62 +16,69 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      onTap: (value) {
-        switch (value) {
-          case 0:
-            context.go(RetipRoute.home);
-            break;
-          case 1:
-            context.go(RetipRoute.explore);
-            break;
-          case 2:
-            context.go(RetipRoute.search);
-            break;
-          case 3:
-            context.go(RetipRoute.library);
-            break;
-          case 4:
-            context.go(RetipRoute.settings);
-            break;
-        }
+    return Theme(
+      data: Theme.of(context).copyWith(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        splashFactory: NoSplash.splashFactory,
+      ),
+      child: BottomNavigationBar(
+        onTap: (value) {
+          switch (value) {
+            case 0:
+              context.go(RetipRoute.home);
+              break;
+            case 1:
+              context.go(RetipRoute.explore);
+              break;
+            case 2:
+              context.go(RetipRoute.search);
+              break;
+            case 3:
+              context.go(RetipRoute.library);
+              break;
+            case 4:
+              context.go(RetipRoute.settings);
+              break;
+          }
 
-        _index = value;
-        setState(() {});
-      },
-      currentIndex: _index,
-      type: BottomNavigationBarType.fixed,
-      showSelectedLabels: true,
-      showUnselectedLabels: true,
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.grey,
-      items: const [
-        BottomNavigationBarItem(
-          activeIcon: Icon(Icons.home),
-          icon: Icon(Icons.home_outlined),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          activeIcon: Icon(Icons.explore),
-          icon: Icon(Icons.explore_outlined),
-          label: 'Explore',
-        ),
-        BottomNavigationBarItem(
-          activeIcon: Icon(Icons.search),
-          icon: Icon(Icons.search_outlined),
-          label: 'Search',
-        ),
-        BottomNavigationBarItem(
-          activeIcon: Icon(Icons.library_music),
-          icon: Icon(Icons.library_music_outlined),
-          label: 'Library',
-        ),
-        BottomNavigationBarItem(
-          activeIcon: Icon(Icons.settings),
-          icon: Icon(Icons.settings_outlined),
-          label: 'Settings',
-        ),
-      ],
+          _index = value;
+          setState(() {});
+        },
+        currentIndex: _index,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.explore),
+            icon: Icon(Icons.explore_outlined),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.search),
+            icon: Icon(Icons.search_outlined),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.library_music),
+            icon: Icon(Icons.library_music_outlined),
+            label: 'Library',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.settings),
+            icon: Icon(Icons.settings_outlined),
+            label: 'Settings',
+          ),
+        ],
+      ),
     );
   }
 }
