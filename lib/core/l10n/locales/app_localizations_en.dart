b00fdf4bcc38934acc19cfa29aefc894d8eeaf5b
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-05-30 06:23:58 +0100

    Bump intl and disable profile mode suffix

---
+++ lib/core/l10n/locales/app_localizations_en.dart
@@ -0,0 +1,143 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get addToQueue => 'Add to queue';
+
+  @override
+  String get addedToFavourites => 'Added to favourites';
+
+  @override
+  String get albums => 'Albums';
+
+  @override
+  String get allowAccess => 'Allow access';
+
+  @override
+  String get artists => 'Artists';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get changeCover => 'Change cover';
+
+  @override
+  String get create => 'Create';
+
+  @override
+  String get createPlaylist => 'Create playlist';
+
+  @override
+  String get deletePlaylist => 'Delete playlist';
+
+  @override
+  String get explore => 'Explore';
+
+  @override
+  String get finish => 'Finish';
+
+  @override
+  String get genres => 'Genres';
+
+  @override
+  String get goToAlbum => 'Go to album';
+
+  @override
+  String get goToArtist => 'Go to artist';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get howItWorks => 'How it works?';
+
+  @override
+  String get howItWorksDescription =>
+      'Retip scans your device for music files and organizes them into albums, artists, and tracks.';
+
+  @override
+  String get library => 'Library';
+
+  @override
+  String get libraryScanned => 'Library scanned';
+
+  @override
+  String get mediaPermission => 'Media permission';
+
+  @override
+  String get newName => 'New name';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get openAppSettings => 'Open app settings';
+
+  @override
+  String get ourMottoDescription =>
+      'If you think that in the era of streaming, the art of offline listening to music has not disappeared. It\'s a sure sign that you need a RETIP!';
+
+  @override
+  String get ourMottto => 'Our motto';
+
+  @override
+  String get playNext => 'Play next';
+
+  @override
+  String get playlistName => 'Playlist name';
+
+  @override
+  String get playlistNameIsRequired => 'Playlist name is required';
+
+  @override
+  String get playlists => 'Playlists';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get removedFromFavourites => 'Removed from favourites';
+
+  @override
+  String get rename => 'Rename';
+
+  @override
+  String get renamePlaylist => 'Rename playlist';
+
+  @override
+  String get saveToPlaylist => 'Save to playlist';
+
+  @override
+  String get savedToPlaylist => 'Saved to playlist';
+
+  @override
+  String get search => 'Search';
+
+  @override
+  String get showInfo => 'Show info';
+
+  @override
+  String get skip => 'Skip';
+
+  @override
+  String get storageAccessIsRequired =>
+      'Storage access is required to play music, please allow access to continue.';
+
+  @override
+  String get tracks => 'Tracks';
+
+  @override
+  String get welcomeToRetip => 'Welcome to Retip!';
+
+  @override
+  String get welcomeToRetipDescription =>
+      'Your music, your way. Enjoy your local music library, offline and uninterrupted.';
+}
