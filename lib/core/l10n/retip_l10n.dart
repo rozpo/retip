9adcd2e893794ce60d29b1e025df536c46b06675
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-05-30 06:51:51 +0100

    Bump intl version

--- lib/core/l10n/retip_l10n.dart
+++ lib/core/l10n/retip_l10n.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'locales/app_localizations.dart';
 
 mixin RetipL10n {
   static AppLocalizations of(BuildContext context) {
