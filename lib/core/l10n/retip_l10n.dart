9e065dadc605c2327158f90c295599a27f733520
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-06-11 18:21:33 +0100

    Migrate to new l10n settings

--- lib/core/l10n/retip_l10n.dart
+++ lib/core/l10n/retip_l10n.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'locales/app_localizations.dart';
+import 'generated/app_localizations.dart';
 
 mixin RetipL10n {
   static AppLocalizations of(BuildContext context) {
