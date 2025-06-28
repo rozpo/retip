b0fd9b48a738abcb66f04361e40ec64e02489b90
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-06-28 01:09:42 +0100

    Add shared preferences provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
+import 'app/data/providers/shared_preferences_provider.dart';
 import 'app/presentation/cubits/dev/dev_cubit.dart';
 import 'app/retip_app.dart';
 import 'core/logger/retip_logger.dart';
@@ -10,16 +11,21 @@ import 'core/utils/run_mode.dart';
 
 final logger = RetipLogger();
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Core
   Bloc.observer = logger.blocObserver;
-
   final router = RetipRouter(observers: [logger.navigatorObserver]);
   final theme = RetipTheme();
 
+  // Providers
+  final sharedPreferencesProvider = await SharedPreferencesProvider.init();
+
+  // Cubits
   final devCubit = DevCubit(isEnabled: RunMode.isRelease == false);
 
+  // App
   final app = RetipApp(
     devCubit: devCubit,
     logger: logger,
