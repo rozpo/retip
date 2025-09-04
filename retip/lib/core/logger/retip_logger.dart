4a8af3f8f021aefc088b75a407405c7c86d3778c
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-09-04 21:13:29 +0100

    Add basic logger

---
+++ retip/lib/core/logger/retip_logger.dart
@@ -0,0 +1,3 @@
+import 'package:talker/talker.dart';
+
+class RetipLogger extends Talker {}
--- retip/lib/main.dart
+++ retip/lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:retip/app/retip_app.dart';
+import 'package:retip/core/logger/retip_logger.dart';
 import 'package:retip/core/router/retip_router.dart';
 import 'package:retip/core/theme/retip_theme.dart';
 
 void main() {
+  final logger = RetipLogger();
+
+  logger.debug('App initialization');
+
   final router = RetipRouter();
   final theme = RetipTheme();
 
   final app = RetipApp(router: router, theme: theme);
 
   runApp(app);
+
+  logger.info('App started');
 }
