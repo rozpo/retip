22d23c979c6f32a329d08c5fed53fdacf7a66911
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-09-06 09:58:53 +0100

    Add bloc observer for logger

--- retip/lib/core/logger/retip_logger.dart
+++ retip/lib/core/logger/retip_logger.dart
@@ -1,15 +1,49 @@
+import 'dart:developer' show log;
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger_observer.dart';
 import 'package:talker_flutter/talker_flutter.dart';
-
-import 'dart:developer' show log;
 
 class RetipLogger extends Talker {
   RetipLogger() : super(logger: TalkerLogger(output: _defaultFlutterOutput));
 
   NavigatorObserver get navigatorObserver => TalkerRouteObserver(this);
 
-  Widget get widget => TalkerScreen(talker: this);
+  BlocObserver get blocObserver => TalkerBlocObserver(talker: this);
+
+  Widget get widget {
+    return TalkerScreen(
+      isLogsExpanded: false,
+      appBarTitle: 'Logger',
+      talker: this,
+    );
+  }
+
+  Widget wrapper({required Widget child}) {
+    return TalkerWrapper(
+      options: TalkerWrapperOptions(
+        enableExceptionAlerts: true,
+        exceptionAlertBuilder: (context, data) {
+          return Container(
+            color: Colors.red,
+            child: Center(child: Text(data.displayException)),
+          );
+        },
+        enableErrorAlerts: true,
+        errorAlertBuilder: (context, data) {
+          return Container(
+            color: Colors.red,
+            child: Center(child: Text(data.displayError)),
+          );
+        },
+      ),
+
+      talker: this,
+      child: child,
+    );
+  }
 
   static void _defaultFlutterOutput(String message) {
     if (kIsWeb) {
--- retip/lib/main.dart
+++ retip/lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:retip/app/retip_app.dart';
 import 'package:retip/core/logger/retip_logger.dart';
 import 'package:retip/core/router/retip_router.dart';
 import 'package:retip/core/theme/retip_theme.dart';
 
 void main() {
+  // Logger initialization
   final logger = RetipLogger();
 
-  logger.debug('App initialization');
-
+  // Startup config read
   const locale = String.fromEnvironment('LOCALE');
   const mode = String.fromEnvironment('MODE');
 
+  logger.debug('App initialization');
   logger.debug('LOCALE=$locale');
   logger.debug('MODE=$mode');
 
+  // Setup app based on config
+  Bloc.observer = logger.blocObserver;
+
+  // Dependency injection
   final router = RetipRouter(logger);
   final theme = RetipTheme();
 
+  // Assemble final app
   final app = RetipApp(
     locale: locale.isNotEmpty ? Locale(locale.toLowerCase()) : null,
     logger: logger,
