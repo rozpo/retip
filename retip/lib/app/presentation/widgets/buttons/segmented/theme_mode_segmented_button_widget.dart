c0bec03bdebdaddb853333ceb90c811e9f7ba30a
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-09-11 20:21:16 +0100

    Fix theme mode state emit

--- retip/lib/app/presentation/widgets/buttons/segmented/theme_mode_segmented_button_widget.dart
+++ retip/lib/app/presentation/widgets/buttons/segmented/theme_mode_segmented_button_widget.dart
@@ -3,49 +3,40 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:retip/app/presentation/cubits/theme/theme_cubit.dart';
 import 'package:retip/core/l10n/retip_l10n.dart';
 
-class ThemeModeSegmentedButtonWidget extends StatefulWidget {
+class ThemeModeSegmentedButtonWidget extends StatelessWidget {
   const ThemeModeSegmentedButtonWidget({super.key});
-
-  @override
-  State<ThemeModeSegmentedButtonWidget> createState() =>
-      _ThemeModeSegmentedButtonWidgetState();
-}
-
-class _ThemeModeSegmentedButtonWidgetState
-    extends State<ThemeModeSegmentedButtonWidget> {
-  var _mode = ThemeMode.system;
 
   @override
   Widget build(BuildContext context) {
     final l10n = RetipL10n.of(context);
 
-    return SegmentedButton<ThemeMode>(
-      expandedInsets: EdgeInsets.symmetric(horizontal: 16),
-      onSelectionChanged: (Set<ThemeMode> newSelection) {
-        setState(() {
-          _mode = newSelection.first;
-        });
-
-        context.read<ThemeCubit>().setThemeMode(_mode);
+    return BlocBuilder<ThemeCubit, ThemeState>(
+      builder: (context, state) {
+        return SegmentedButton<ThemeMode>(
+          expandedInsets: EdgeInsets.symmetric(horizontal: 16),
+          onSelectionChanged: (Set<ThemeMode> newSelection) {
+            context.read<ThemeCubit>().setThemeMode(newSelection.first);
+          },
+          segments: [
+            ButtonSegment<ThemeMode>(
+              value: ThemeMode.system,
+              icon: Icon(Icons.brightness_4),
+              label: Text(l10n.system),
+            ),
+            ButtonSegment<ThemeMode>(
+              value: ThemeMode.light,
+              icon: Icon(Icons.light_mode),
+              label: Text(l10n.light),
+            ),
+            ButtonSegment<ThemeMode>(
+              value: ThemeMode.dark,
+              icon: Icon(Icons.dark_mode),
+              label: Text(l10n.dark),
+            ),
+          ],
+          selected: {state.themeMode},
+        );
       },
-      segments: [
-        ButtonSegment<ThemeMode>(
-          value: ThemeMode.system,
-          icon: Icon(Icons.brightness_4),
-          label: Text(l10n.system),
-        ),
-        ButtonSegment<ThemeMode>(
-          value: ThemeMode.light,
-          icon: Icon(Icons.light_mode),
-          label: Text(l10n.light),
-        ),
-        ButtonSegment<ThemeMode>(
-          value: ThemeMode.dark,
-          icon: Icon(Icons.dark_mode),
-          label: Text(l10n.dark),
-        ),
-      ],
-      selected: {_mode},
     );
   }
 }
