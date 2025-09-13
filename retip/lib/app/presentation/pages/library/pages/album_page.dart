4b90314b160fc2d2397b37d8c43b625740f88661
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-09-13 07:25:44 +0100

    Update album page

--- retip/lib/app/presentation/pages/library/pages/album_page.dart
+++ retip/lib/app/presentation/pages/library/pages/album_page.dart
@@ -13,32 +13,70 @@ class AlbumPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final shortestSide = MediaQuery.of(context).size.shortestSide;
+
     return Scaffold(
-      appBar: AppBar(title: Text('Album $id')),
-      body: BlocBuilder<TrackBloc, TrackState>(
-        bloc: TrackBloc(context.read<TrackRepository>())
-          ..add(TrackFetchByAlbumEvent(id)),
-        builder: (context, state) {
-          final tracks = <TrackEntity>[];
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.shuffle),
+      ),
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+            ],
+            stretch: true,
+            expandedHeight: shortestSide,
+            flexibleSpace: FlexibleSpaceBar(
+              expandedTitleScale: 1.25,
+              collapseMode: CollapseMode.pin,
+              centerTitle: true,
+              title: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text('Album $id', textAlign: TextAlign.center),
+              ),
+              background: Center(
+                child: Card(
+                  child: SizedBox.square(
+                    dimension: shortestSide / 2,
+                    child: Icon(Icons.album, size: shortestSide / 3),
+                  ),
+                ),
+              ),
+            ),
+            pinned: true,
+          ),
+          BlocBuilder<TrackBloc, TrackState>(
+            bloc: TrackBloc(context.read<TrackRepository>())
+              ..add(TrackFetchByAlbumEvent(id)),
+            builder: (context, state) {
+              final tracks = <TrackEntity>[];
 
-          if (state is TrackIdleState) {
-            tracks.addAll(state.tracks);
-          }
+              if (state is TrackIdleState) {
+                tracks.addAll(state.tracks);
+              }
 
-          return ListView.builder(
-            itemCount: tracks.length,
-            itemBuilder: (context, index) {
-              final track = tracks[index];
+              return SliverList.builder(
+                itemCount: tracks.length,
+                itemBuilder: (context, index) {
+                  final track = tracks[index];
 
-              return TrackListTileWidget(
-                track,
-                onTap: () {
-                  context.read<AudioService>().setQueue(tracks, index: index);
+                  return TrackListTileWidget(
+                    track,
+                    onTap: () {
+                      context.read<AudioService>().setQueue(
+                        tracks,
+                        index: index,
+                      );
+                    },
+                  );
                 },
               );
             },
-          );
-        },
+          ),
+        ],
       ),
     );
   }
